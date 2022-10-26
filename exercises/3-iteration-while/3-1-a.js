8e228893b2d12e4dfa928786bcce99e2de04be7b
Lennart Dowlat Khah <lennart.dowlat.khah@student.ehb.be>
2022-10-26 09:32:45 +0200

    les 4

---
+++ exercises/3-iteration-while/3-1-a.js
@@ -0,0 +1,33 @@
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+"use strict";
+
+console.log(context)
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawLines();
+}
+
+function drawLines() {
+    let i = 0;
+    while (i<6) {
+        //This drawLine function does not exist yet!
+        //Add it to the utils script:
+        //The function draws a line between 2 points
+        //And has the following parameters: x1, y1, x2, y2
+
+        console.log ("loup", i); 
+
+        Utils.drawLine(75,75 +25 *i , 325, 75 + 25 * i);
+        i++;
+        
+        
+    }
+}
---
+++ exercises/3-iteration-while/3-1-b.js
@@ -0,0 +1,24 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawGrid();
+}
+
+function drawGrid() {
+    let i = 0;
+    while (i < 6) {
+        let x = 75;
+        let y = x ;
+        Utils.drawLine(75, y +50 *i, 325, y +50*i);
+        Utils.drawLine(x +50 *i, 75, x+50*i, 325);
+        i++;
+    }
+}
